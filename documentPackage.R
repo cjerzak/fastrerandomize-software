96dc0273f11d2f7a98b4b1a29f4aa5d7e5dd6300
cjerzak <connor.jerzak@gmail.com>
2023-09-20 11:59:54 -0500

    add

---
+++ documentPackage.R
@@ -0,0 +1,15 @@
+rm(list=ls())
+package_name <- "fastrerandomize"
+setwd(sprintf("~/Documents/%s-software", package_name))
+
+package_path <- sprintf("~/Documents/%s-software/%s",package_name,package_name)
+
+devtools::document(package_path)
+try(file.remove(sprintf("./%s.pdf",package_name)),T)
+system(sprintf("R CMD Rd2pdf %s",package_path))
+
+# install.packages( sprintf("~/Documents/%s-software/%s",package_name,package_name),repos = NULL, type = "source")
+# library( causalimages ); data(  CausalImagesTutorialData )
+log(sort( sapply(ls(),function(l_){object.size(eval(parse(text=l_)))})))
+
+
