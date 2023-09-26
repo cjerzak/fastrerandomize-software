3f69c576af31080627057f677eb27c16012a6453
cjerzak <connor.jerzak@gmail.com>
2023-09-26 08:30:20 -0500

    up

--- fastrerandomize/R/FRR_InitializeJAX.R
+++ fastrerandomize/R/FRR_InitializeJAX.R
@@ -18,20 +18,14 @@
 #' @md
 
 InitializeJAX <- function(conda_env, conda_env_required){
-  library(fastmatch);
+  print("Attempting JAX configuration...")
+  {
+  #library(fastmatch);
   library(reticulate)
   reticulate::use_condaenv(condaenv = conda_env, required = conda_env_required)
 
-  #print(   tf$config$list_physical_devices('GPU')  )
-  #tfFloatType <- tf$float64
-  #tf$keras$backend$set_floatx('float64') #default backend
-
-  #tf setup - set seed
-  #tf$random$set_seed(as.integer(round(runif(1,min=0,max=10^6),0))); print(  tf$version$VERSION )
-  #tfp <- tf_probability();tfd <-tfp$distributions;tfb <-tfp$bijectors
-
-  # setup other r packages
-  Sys.sleep(2L)
+  # setup packages
+  Sys.sleep(1L)
   jax <<- reticulate::import("jax")
   jnp <<- reticulate::import("jax.numpy")
   np <<- reticulate::import("numpy")
@@ -50,8 +44,7 @@ InitializeJAX <- function(conda_env, conda_env_required){
   SoftPlus_r <<- function(x){ log(exp(x)+1) }
 
   LinearizeNestedList <<- function (NList, LinearizeDataFrames = FALSE, NameSep = "/",
-                                   ForceNames = FALSE)
-  {
+                                   ForceNames = FALSE){
     stopifnot(is.character(NameSep), length(NameSep) == 1)
     stopifnot(is.logical(LinearizeDataFrames), length(LinearizeDataFrames) ==
                 1)
@@ -110,4 +103,140 @@ InitializeJAX <- function(conda_env, conda_env_required){
     }
     return(NList)
   }
+  }
+  print("Success configuring JAX!")
+
+  print("Attempting setup of core JAX functions...")
+  {
+    expand_grid_jax <<- function(...){
+      #grid <- jnp$meshgrid(jnp$array(c(0,1L)), jnp$array(c(0,1L)), jnp$array(c(0,1L)), indexing = "ij")
+      grid <- jnp$meshgrid(..., indexing='ij')
+      grid <- jnp$vstack(lapply(grid,function(zer){jnp$ravel(zer)}))
+      grid <- jnp$transpose(grid)
+      return( grid )
+    }
+
+    if(T == F){
+      n_units <- 35
+      expand_grid_jax_text <- paste(rep("jnp$array(0L:1L)",times = n_units), collapse = ", ")
+      expand_grid_text <- paste(rep("0L:1L",times = n_units), collapse = ", ")
+      system.time( tmp1 <- eval(parse(text = sprintf("expand_grid_jax(%s)",expand_grid_jax_text))) )
+      system.time( tmp2 <- eval(parse(text = sprintf("expand.grid(%s)",expand_grid_text))) )
+      tmp1$shape
+      dim( tmp2 )
+    }
+
+    FastHotel2T2 <<- jax$jit( function(samp_, w_, n0, n1){
+      # set up calc
+      RowBroadcast <- jax$vmap(function(mat, vec){
+        jnp$multiply(mat, vec)},
+        in_axes = list(1L, NULL))
+      xbar1 <- jnp$divide(jnp$sum(RowBroadcast(samp_,w_),1L,keepdims = T), n1)
+      xbar2 <- jnp$divide(jnp$sum(RowBroadcast(samp_,jnp$subtract(1.,w_)),1L,keepdims = T), n0)
+      CovPooled <- jnp$cov(samp_, rowvar = F)
+      CovWts <- jnp$add(jnp$reciprocal(n0), jnp$reciprocal(n1))
+      xbar_diff <- jnp$subtract(xbar1, xbar2)
+
+      # perform calc
+      Tstat <- jnp$matmul(
+        jnp$matmul(jnp$transpose(xbar_diff),
+                   jnp$linalg$inv( jnp$multiply(CovPooled,CovWts) )),
+        xbar_diff)
+    })
+
+    VectorizedFastHotel2T2 <<- jax$jit( jax$vmap(function(samp_, w_, n0, n1){
+      FastHotel2T2(samp_, w_, n0, n1)},
+      in_axes = list(NULL, 0L, NULL, NULL)) )
+
+    if(T == F){
+      # tests
+      Compositional::hotel2T2()
+      samp_ <- jnp$array(matrix(rnorm(10*100),nrow=100))
+      w_ <- jnp$array(sample(c(0,1), size = 100, replace =T))
+      w_full <- jnp$array(matrix(rbinom(100*34,size = 1, prob = 0.5),nrow = 100))
+      FastHotel2T2(samp_ = samp_, w_ = w_, n0 = n0, n1 = n1)
+      VectorizedFastHotel2T2(samp_, w_full, n0, n1)
+    }
+
+    FastDiffInMeans <<- jax$jit( function(y_,w_, n0, n1){
+      my1 <- jnp$divide(jnp$sum(jnp$multiply(y_, w_)), n1)
+      my0 <- jnp$divide(jnp$sum(jnp$multiply(y_, jnp$subtract(1.,w_))), n0)
+      return( diff10 <- jnp$subtract(my1, my0) )
+    })
+
+    VectorizedTakeAxis0 <<- jax$jit( function(A_, I_){
+      jnp$expand_dims(jnp$take(A_, I_, axis = 0L), 0L)
+    })
+
+    Potentisl2Obs <<- jax$jit(function(Y0__, Y1__, obsW__){
+      jnp$add( jnp$multiply(Y0__, jnp$subtract(1, obsW__)),
+               jnp$multiply(Y1__, obsW__))
+    })
+
+    W_VectorizedFastDiffInMeans <<- jax$jit( jax$vmap(function(y_, w_, n0, n1){
+      FastDiffInMeans(y_, w_, n0, n1)},
+      in_axes = list(NULL, 0L, NULL, NULL)) )
+
+    Y_VectorizedFastDiffInMeans <<- jax$jit( jax$vmap(function(y_, w_, n0, n1){
+      FastDiffInMeans(y_, w_, n0, n1)},
+      in_axes = list(0L, NULL, NULL, NULL)) )
+
+    YW_VectorizedFastDiffInMeans <<- jax$jit( jax$vmap(function(y_, w_, n0, n1){
+      W_VectorizedFastDiffInMeans(y_, w_, n0, n1)},
+      in_axes = list(0L, NULL, NULL, NULL)) )
+
+    WVectorizedFastDiffInMeans <<- jax$jit( jax$vmap(function(y_, w_, n0, n1){
+      FastDiffInMeans(y_, w_, n0, n1)},
+      in_axes = list(NULL, 0L, NULL, NULL)) )
+
+    GreaterEqualMagCompare <<- jax$jit(function(NULL_, OBS_){
+      jnp$mean(jnp$greater_equal(jnp$abs(NULL_),  jnp$expand_dims(OBS_,1L)), 1L)
+    })
+
+    if(T == F){
+      # tests
+      y_ <- jnp$array(rnorm(10))
+      w_ <-  jnp$array(rbinom(10,size=1, prob = 0.5))
+      n0 <- jnp$array(10.)
+      n1 <- jnp$array(3.)
+      w_mat <- jnp$array( matrix(rbinom(100*10, size = 1, prob = 0.5),nrow = 100) )
+      FastDiffInMeans(y_, w_, n0, n1)
+      np$array( VectorizedFastDiffInMeans(y_, w_mat, n0, n1) )
+    }
+
+    get_stat_vec_at_tau_pseudo <<- jax$jit( function(treatment_pseudo,
+                                                     obsY_array,
+                                                     obsW_array,
+                                                     tau_pseudo,
+                                                     n0_array,
+                                                     n1_array){
+      #Y0_under_null <- obsY - obsW*tau_pseudo
+      Y0_under_null <- jnp$subtract(obsY_array,  jnp$multiply(obsW_array, tau_pseudo))
+
+      #Y1_under_null_pseudo <- Y0_under_null + treatment_pseudo*tau_pseudo
+      Y1_under_null_pseudo <- jnp$add(Y0_under_null,  jnp$multiply(treatment_pseudo, tau_pseudo))
+
+      #Yobs_pseudo <- Y1_under_null_pseudo*treatment_pseudo + Y0_under_null * (1-treatment_pseudo)
+      Yobs_pseudo <- jnp$add(jnp$multiply(Y1_under_null_pseudo,treatment_pseudo),
+                             jnp$multiply(Y0_under_null, jnp$subtract(1., treatment_pseudo)))
+
+      #stat_ <- mean(Yobs_pseudo[treatment_pseudo == 1]) - mean(Yobs_pseudo[treatment_pseudo == 0])
+      stat_ <- FastDiffInMeans(Yobs_pseudo, treatment_pseudo, n0_array, n1_array)
+    } )
+
+    vec1_get_stat_vec_at_tau_pseudo <<- jax$jit( jax$vmap(function(treatment_pseudo,
+                                                                   obsY_array,
+                                                                   obsW_array,
+                                                                   tau_pseudo,
+                                                                   n0_array,
+                                                                   n1_array){
+      get_stat_vec_at_tau_pseudo(treatment_pseudo,
+                                 obsY_array,
+                                 obsW_array,
+                                 tau_pseudo,
+                                 n0_array,
+                                 n1_array)
+    }, in_axes = list(0L, NULL, NULL, NULL, NULL, NULL)) )
+  }
+  print("Success setting up core JAX functions!")
 }
