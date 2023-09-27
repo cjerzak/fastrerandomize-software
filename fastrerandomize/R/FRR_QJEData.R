4921e21be34679f81dde4b9f3aa1faa76b013c3b
cjerzak <connor.jerzak@gmail.com>
2023-09-27 17:44:51 -0500

    up

---
+++ fastrerandomize/R/FRR_QJEData.R
@@ -0,0 +1,12 @@
+#' QJEData
+#'
+#' The dataset originates from the study "Moral hazard: Experimental evidence from tenancy contracts" by
+#' Burchardi, Konrad B et al., published in "The Quarterly Journal of Economics" in 2019 (Volume 134, Issue 1, Pages 281-347).
+#'
+#' @format A data frame with 968 rows and many columns containing treatment data for a Quarterly Journal of Economics experiment on agriculture.
+
+#' @source Burchardi, Konrad B et al. (2019). "Moral hazard: Experimental evidence from tenancy contracts."
+#' In: _The Quarterly Journal of Economics_ 134.1, pp. 281–347
+#'
+
+"QJEData"
