121488724f6a6e95f5df6203ddfde65ced5bf837
Connor Jerzak <cjerzak@dhcp-10-250-186-204.harvard.edu>
2024-11-17 15:23:11 -0500

    tests up

--- tests/RunAllTutorials_TestSuite.R
+++ tests/RunAllTutorials_TestSuite.R
@@ -13,7 +13,7 @@
     t_Initialize <- try({
       fastrerandomize::InitializeJAX(conda_env = "jax_cpu", conda_env_required = T)
     },T)
-    if("try-error" %in% class(t_GenData)){ stop("Failed at t_Initialize...") }
+    if("try-error" %in% class(t_Initialize)){ stop("Failed at t_Initialize...") }
     
     t_GenData <- try({
       set.seed(999L, kind = "Wichmann-Hill")
@@ -31,8 +31,17 @@
           randomization_type="exact",
           max_draws=1000)
       },T)
-      
       if("try-error" %in% class(t_GetSet)){ stop(sprintf("Failed at t_GetSet: %s...",type_)) }
+      
+      t_RRTest <- try({
+        RandomizationSet_ <- fastrerandomize::RandomizationTest(
+          obsW = RandomizationSet_[1,],
+          obsY = rnorm(nrow(RandomizationSet_)),
+          candidate_randomizations = RandomizationSet_,
+          X = X,
+          max_draws=1000)
+      },T)
+      if("try-error" %in% class(t_RRTest)){ stop(sprintf("Failed at t_RRTest: %s...",type_)) }
     }
   }, T)
     
