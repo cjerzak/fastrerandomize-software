1af82d21534b9ab108ad2bc125581999bdc9e5e2
Connor Jerzak <cjerzak@dhcp-10-250-186-204.harvard.edu>
2024-11-17 15:14:56 -0500

    up

---
+++ tests/RunAllTutorials_TestSuite.R
@@ -0,0 +1,41 @@
+#!/usr/bin/env Rscript
+{
+  ##########################################
+  # code for testing functionalities of fastrerandomize on your hardware
+  ##########################################
+  tryTests <- try({
+    # remote install latest version of the package
+    # devtools::install_github(repo = "cjerzak/fastrerandomize-software/fastrerandomize")
+
+    # local install for development team
+    # install.packages("~/Documents/fastrerandomize-software/fastrerandomize",repos = NULL, type = "source",force = F)
+    
+    t_Initialize <- try({
+      fastrerandomize::InitializeJAX(conda_env = "jax_cpu", conda_env_required = T)
+    },T)
+    if("try-error" %in% class(t_GenData)){ stop("Failed at t_Initialize...") }
+    
+    t_GenData <- try({
+      set.seed(999L, kind = "Wichmann-Hill")
+      X <- matrix(rnorm(100*5), 100, 5)
+    },T)
+    if("try-error" %in% class(t_GenData)){ stop("Failed at t_GenData...") }
+    
+    for(type_ in c("exact","monte_carlo")){ 
+      t_GetSet <- try({
+        RandomizationSet_ <- fastrerandomize::GenerateRandomizations(
+          n_units = 100,
+          n_treated = 50,
+          X = X,
+          randomization_accept_prob=0.1,
+          randomization_type="exact",
+          max_draws=1000)
+      },T)
+      
+      if("try-error" %in% class(t_GetSet)){ stop(sprintf("Failed at t_GetSet: %s...",type_)) }
+    }
+  }, T)
+    
+  if('try-error' %in% class(tryTests)){ print("At least one test failed..."); print( tryTests ) }
+  if(!'try-error' %in% class(tryTests)){ print("All tests succeeded!") }
+}
