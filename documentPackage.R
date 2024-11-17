45e6d105fd981b58de8df9fa3c616857a626eab8
Connor Jerzak <cjerzak@dhcp-10-250-186-204.harvard.edu>
2024-11-17 13:36:34 -0500

    add warren + aniket in description

--- documentPackage.R
+++ documentPackage.R
@@ -1,21 +1,23 @@
-rm(list=ls())
-# set path and specify package name
-setwd(sprintf("~/Documents/%s-software",
-              package_name <- "fastrerandomize"))
-
-package_path <- sprintf("~/Documents/%s-software/%s",package_name,package_name)
-tools::add_datalist(package_path, force = TRUE, small.size = 1L)
-devtools::document(package_path)
-
-# remove old PDF
-try(file.remove(sprintf("./%s.pdf",package_name)),T)
-
-# create new PDF
-system(sprintf("R CMD Rd2pdf %s",package_path))
-
-# install.packages( sprintf("~/Documents/%s-software/%s",package_name,package_name),repos = NULL, type = "source")
-# library( causalimages ); data(  CausalImagesTutorialData )
-log(sort( sapply(ls(),function(l_){object.size(eval(parse(text=l_)))})))
-
-# Check package to ensure it meets CRAN standards.
-# devtools::check( package_path )
+{
+  rm(list=ls())
+  # set path and specify package name
+  setwd(sprintf("~/Documents/%s-software",
+                package_name <- "fastrerandomize"))
+  
+  package_path <- sprintf("~/Documents/%s-software/%s",package_name,package_name)
+  tools::add_datalist(package_path, force = TRUE, small.size = 1L)
+  devtools::document(package_path)
+  
+  # remove old PDF
+  try(file.remove(sprintf("./%s.pdf",package_name)),T)
+  
+  # create new PDF
+  system(sprintf("R CMD Rd2pdf %s",package_path))
+  
+  # install.packages( sprintf("~/Documents/%s-software/%s",package_name,package_name),repos = NULL, type = "source")
+  # library( causalimages ); data(  CausalImagesTutorialData )
+  log(sort( sapply(ls(),function(l_){object.size(eval(parse(text=l_)))})))
+  
+  # Check package to ensure it meets CRAN standards.
+  # devtools::check( package_path )
+}
